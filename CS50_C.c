f6e3c1bfe9b07d69d5c1f24988e2c26b2d9d13fd
Katleho-Nyoni <eaglenyoni@gmail.com>
2025-03-27 19:44:40 +0200

    Conditonal Statements intro

--- CS50_C.c
+++ CS50_C.c
@@ -5,5 +5,23 @@ int main(void)
     char Name[] = "Katleho";
     printf("Hello, %s \n", Name);
 
+    // Intergers
+    int num = 7;
+    int numm = 12;
+    printf("The lucky number is %i \n", num);
+
+    // Conditional Statements
+    if (num > num)
+    {
+        printf("The luck number %i is greater than the hidden number %i \n", num, numm);
+    }
+    else if (num < numm)
+    {
+        printf("The luck number %i is less than the hidden number %i \n", num, numm);
+    }
+    else
+    {
+        printf("The luck number %i is equal to the hidden number %i \n", num, numm);
+    }
     return 0;
 }

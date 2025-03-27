029d2e59de3592caca9ef1b4d8635671d1fcfc58
Katleho-Nyoni <eaglenyoni@gmail.com>
2025-03-27 23:25:35 +0200

    For loops

--- CS50_C.c
+++ CS50_C.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 
+void Smile(void);
+
 int main(void)
 {
-    char Name[] = "Katleho";
-    printf("Hello, %s \n", Name);
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Hello, world!\n");
+    }
 
-    // Intergers
-    int num = 7;
-    int numm = 12;
-    printf("The lucky number is %i \n", num);
+    for (int i = 0; i < 2; i++)
+    {
+        Smile();
+    }
 
-    // Conditional Statements
-    if (num > num)
-    {
-        printf("The luck number %i is greater than the hidden number %i \n", num, numm);
-    }
-    else if (num < numm)
-    {
-        printf("The luck number %i is less than the hidden number %i \n", num, numm);
-    }
-    else
-    {
-        printf("The luck number %i is equal to the hidden number %i \n", num, numm);
-    }
     return 0;
+}
+
+void Smile(void)
+{
+    printf("Smile!\n");
 }

4286280e0efac3627ec0539f0a3092bb772f05c9
Katieum <eaglenyoni@gmail.com>
2025-06-29 03:44:42 +0200

    2D Array & nested for loop

--- Relearning-C++/Array.cpp
+++ Relearning-C++/Array.cpp
@@ -11,5 +11,20 @@ int main()
     int size = sizeof(arr) / sizeof(arr[0]);
     cout << "The size of the array is: " << size << endl;
 
+    /* 2D ARRAYS*/
+    int arr2D[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}};
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << arr2D[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }

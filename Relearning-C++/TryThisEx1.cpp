10093ccc4b44c70192ef7c0611dbb01a073b0957
Katieum <eaglenyoni@gmail.com>
2025-10-24 21:55:26 +0200

    Initializing this project

---
+++ Relearning-C++/TryThisEx1.cpp
@@ -0,0 +1,18 @@
+/*
+This is a Try This Exercise from
+Programming: Principles and Practice Using C++ Second Edition by Bjarne Stroustrup
+Chapter 3: Objects, Types, and Values
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    using namespace std;
+    char b = char('a'+1);
+    char a = 'a';
+    cout << a << "\n";
+    cout << b << "\n";
+    return 0;
+}

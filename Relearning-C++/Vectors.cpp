a08a14a69ba1b8f95cc34b0397ec79035ee069bf
Katieum <eaglenyoni@gmail.com>
2025-10-25 20:12:35 +0200

    Not so cute error

--- Relearning-C++/Vectors.cpp
+++ Relearning-C++/Vectors.cpp
@@ -9,7 +9,24 @@
 #include <string>
 #include <vector>
 
+using namespace std;
+
 int main(){
+
+    vector<int> v0 = {5,7,9,4,6,8};
+    
+    for(int i = 0; i < v0.size(); ++i){
+        cout << v0[i] << '\n';
+        for(int i = 0; i < v0.size(); ++i){
+        cout << i << '\n';
+        }
+    }
+
+    printf("************************************************** \n");
+
+    for(int x: v0){
+        cout << x << '\n';
+    }
 
     return 0;
 }
